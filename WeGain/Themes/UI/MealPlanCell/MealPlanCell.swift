5fc993041b359da51bb16b6000a223caab75e6b6
vincent-chandra <vincentchandra1@gmail.com>
2021-06-09 19:53:11 +0700

    feat: adding reusable cell

--- WeGain/Themes/UI/MealPlanCell/MealPlanCell.swift
+++ WeGain/Themes/UI/MealPlanCell/MealPlanCell.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class MealPlanCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var mealPlanCellNameLabel: UILabel!
+    @IBOutlet weak var mealPlanCellKCalLabel: UILabel!
+    @IBOutlet weak var mealPlanCellCheckButton: UIButton!
+    @IBOutlet weak var mealPlanCellView: UIView!
+    
     
 }
