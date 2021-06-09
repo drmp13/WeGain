f00bbb8b48e532bfbebce6c5d7b7f9fb4ee4b713
vincent-chandra <vincentchandra1@gmail.com>
2021-06-09 18:31:19 +0700

    feat: add table cell + image assets

---
+++ WeGain/Themes/UI/CalendarCell/CalendarCell.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarCell.swift
+//  WeGain
+//
+//  Created by Vincent on 09/06/21.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
