a06b11efb52f44918c4c9cefe8117c3c67dce1f1
DRMP <drizkimp@gmail.com>
2021-06-16 18:57:07 +0700

    Dashboard meal plan --update

--- WeGain/Features/MealPlan/Model/PlanDataStore.swift
+++ WeGain/Features/MealPlan/Model/PlanDataStore.swift
@@ -22,6 +22,27 @@ struct PlanDataStore {
         
         return fetchedResult
     }
+    
+    func fetchByDate(date: Date, type: PlanType) -> [Plan] {
+
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Plan")
+        let predicateDateYear = NSPredicate(format: "date == %@", date as CVarArg)
+        let predicateCategory = NSPredicate(format: "type == %@", type.rawValue)
+        request.predicate = NSCompoundPredicate(type: NSCompoundPredicate.LogicalType.and, subpredicates: [predicateDateYear,predicateCategory])
+        request.sortDescriptors = [NSSortDescriptor(key:"date", ascending:false)]
+        
+        let context = PersistenceManager.shared.persistentContainer.viewContext
+        
+        var fetchedResult: [Plan] = []
+        do {
+            fetchedResult = try context.fetch(request) as! [Plan]
+        } catch {
+            print("Could not fetch plan data")
+        }
+
+
+      return fetchedResult
+    }
 }
 
 
